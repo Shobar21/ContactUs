8c4ccb579dddcc2a220bed863d60fab2e5a1a8f8
shoba rohra <rshoba822@gmail.com>
2024-09-19 20:29:18 +0500

    img

--- project2/src/components/Form.jsx
+++ project2/src/components/Form.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { FaPhoneAlt, FaComments } from 'react-icons/fa'
+import img from '../images/image.png'
 
 function Form() {
   return (
-    <div className='Form'>
-      <button className='contact-button'>
-        <FaComments size={24} /> {/* Chat icon */}
-        VIA SUPPORT CHAT
-      </button>
-      <button className='contact-button '>
-        <FaPhoneAlt size={24} /> {/* Phone icon */}
-        VIA CALL
-      </button>
+    <div className='container'>
+      <div className='Form'>
+        <button className='contact-button'>
+          <FaComments size={24} /> {/* Chat icon */}
+          VIA SUPPORT CHAT
+        </button>
+        <button className='contact-button '>
+          <FaPhoneAlt size={24} /> {/* Phone icon */}
+          VIA CALL
+        </button>
+      </div>
+      <div className='form-img'>
+        <img src={img} alt='' />
+      </div>
     </div>
   )
 }
